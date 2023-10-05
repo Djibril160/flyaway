a4cda0d8c0c046af0faeef933e1ae711ad5b0fc8
djibril soukouna <djib_cisse_9@hotmail.com>
2023-10-05 16:03:26 +0200

    initialisation

---
+++ Frontend/components/Connexion.js
@@ -0,0 +1,15 @@
+import styles from '../styles/Home.module.css';
+
+function Connexion() {
+  return (
+    <div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+export default Connexion;
